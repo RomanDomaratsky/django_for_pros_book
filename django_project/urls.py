d7b32c29c51faada2ef2bf2560c6c04fdc176f26
RomanDomaratsky <roma.domaratsky@gmail.com>
2024-08-19 21:10:15 +0300

    ch17

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('dickhead/', admin.site.urls),
 
     path('accounts/', include('allauth.urls')),
 
